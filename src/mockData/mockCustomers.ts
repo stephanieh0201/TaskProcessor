63565b3f6d32b3a746252afca45e76519f618efe
Stephanie Verlingo <sverlingo@gmail.com>
2019-07-17 21:10:02 -0400

    Add mockdata and configuration file

---
+++ src/mockData/mockCustomers.ts
@@ -0,0 +1,17 @@
+import { CustomerList } from '../components/customer/Customer';
+
+export const mockCustomers: CustomerList = {
+  customers: [{
+    _id: '1',
+    taskMinSeconds: 0,
+    taskMaxSeconds: 4,
+  }, {
+    _id: '2',
+    taskMinSeconds: 3,
+    taskMaxSeconds: 5,
+  }, {
+    _id: '3',
+    taskMinSeconds: 10,
+    taskMaxSeconds: 16,
+  }],
+};
